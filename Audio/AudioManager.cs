a60d55bc0819ba2244f8bfc99d126390b86f3305
Rachellie <43972385+Rachellie@users.noreply.github.com>
2020-09-13 17:26:41 -0700

    Uploaded Audio scripts

---
+++ Audio/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Debug.Log("Play theme song");
+        Play("theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s != null)
+        {
+            s.source.Play();
+        }
+    }
+}
