c8afe6330a2056d270df8c13be87d63fe4fcfb58
Rachellie <43972385+Rachellie@users.noreply.github.com>
2020-09-13 17:31:00 -0700

    Uploaded RecipeBook scripts

---
+++ RecipeBook/ButtonTab.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTab : MonoBehaviour
+{
+    public Button button;
+	public int tabNum;
+    public Text textLabel;
+	
+	private SliderMenuAnim sliderMenu;
+	
+	void Start()
+    {
+        button.onClick.AddListener(HandleClick);
+    }
+	
+	public void Setup(SliderMenuAnim slider, int num)
+	{
+		tabNum = num;
+		sliderMenu = slider;
+		textLabel.text = num.ToString();
+	}
+	
+	public void HandleClick()
+	{
+		Debug.Log("button " + textLabel.text + " clicked");
+		sliderMenu.ClickTabButton(button, tabNum);
+	}
+}
---
+++ RecipeBook/IngredientInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientInfo : MonoBehaviour
+{
+	public Text nameLabel;
+	public Image iconImage;
+
+    public void Setup(FoodObject currentFood)
+	{
+		nameLabel.text = currentFood.getName() + " x" + currentFood.getQuantity().ToString(); ;
+		iconImage.sprite = Resources.Load<Sprite>(currentFood.getIcon());
+	}
+}
